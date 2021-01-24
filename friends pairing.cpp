93915421e27a3406005d8f5028141d1796cda184
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-01-24 07:11:18 +0530

    Create friends pairing.cpp

---
+++ friends pairing.cpp
@@ -0,0 +1,17 @@
+
+// formula used => f(n) = f(n-1) + (n-1)*f(n-1);
+//using recursive tree reach the approach
+
+#include<bits/stdc++.h>
+ using namespace std;
+ int main(){
+     int n;
+      cin>>n;
+      int dp[n+1];
+      dp[1] = 1;
+      dp[2] = 2;
+      for(int i =3;i<=n;i++){
+          dp[i] = dp[i-1] + (i-1)*dp[i-2];
+      }
+      cout<<dp[n];
+ }
