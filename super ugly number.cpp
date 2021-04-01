a494d56c903c5ccac98527241bb37eb042003c73
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-04-01 16:37:09 +0530

    Create super ugly number.cpp

---
+++ super ugly number.cpp
@@ -0,0 +1,16 @@
+ int nthSuperUglyNumber(int k, vector<int>& p) {
+        
+         long int n = p.size();
+         vector<long int> dp(k,INT_MAX);
+        vector<long int>last(n,0);
+        dp[0] =1;
+        for(long int i = 1; i<k;i++){
+            for(long int j =0;j<n;j++){
+                dp[i] = min(dp[i],dp[last[j]]*p[j]);
+            }
+             for(int j =0;j<n;j++){
+                if(dp[i] == dp[last[j]]*p[j]) last[j]++;
+            }
+        }
+        return dp[k-1];
+    }
